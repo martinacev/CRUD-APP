b0451cfa8a0a87d439a599c6a41d1b4c9d3e80ef
Martin <martin_acevski@outlook.com>
2024-06-12 12:29:09 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Input from "./components/input/Input";
+
+function App() {
+	return (
+		<div className="container">
+			<Input />
+		</div>
+	);
+}
+
+export default App;
